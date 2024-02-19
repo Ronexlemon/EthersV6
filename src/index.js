32ce001c6ca3dc9e36ed464ccffc57695f7f0b3f
RonexLemon <ronexondimu@gmail.com>
2024-02-19 20:09:38 +0300

    maxmum uint

--- src/index.js
+++ src/index.js
@@ -69,10 +69,16 @@ async function AbiEncoder(){
 async function constant(){
   const minint256 = ethers.MinInt256;
 
+  const maxint256 = ethers.MaxInt256;
   
   console.log("minInt256",minint256)
+  console.log("maxmum int",maxint256)
 
-  
+
+  const maxuint256 = ethers.MaxUint256;
+  console.log("maxmum uint:",maxuint256)
+
+
 
   const zeroAddress = ethers.ZeroAddress;
   console.log("addressmzero:",zeroAddress)
